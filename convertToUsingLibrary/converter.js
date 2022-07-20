6106833245b6b0f1774210b3afb8e72018ee23e4
siminaB <simina.baumgarten@gmail.com>
2022-07-20 15:05:49 +0300

    functional with myConverter

---
+++ convertToUsingLibrary/converter.js
@@ -0,0 +1,120 @@
+"use strict";
+export default class myConverter{
+
+BIN_DICTIONARY = { 
+        '0': "0000",
+        '1': "0001",
+        '2': "0010",
+        '3': "0011",
+        '4': "0100",
+        '5': "0101",
+        '6': "0110",
+        '7': "0111",
+        '8': "1000",
+        '9': "1001",
+        'a': "1010",
+        'b': "1011",
+        'c': "1100",
+        'd': "1101",
+        'e': "1110",
+        'f': "1111"
+    };
+
+HEXA_DICTIONARY = {
+    'A': 10,
+    'B': 11,
+    'C': 12,
+    'D': 13,
+    'E': 14,
+    'F': 15,
+};
+
+TO_HEXA_DICTIONARY = {
+    10 : 'A' ,
+    11 : 'B',
+    12 : 'C',
+    13 : 'D',
+    14 :'E',
+    15 : 'F',
+};
+
+base;
+
+constructor(base){
+   this.base=base;
+};
+
+convertToBinary(iV) {
+    let result = "";
+    
+    for (let i = 0; i < iV.length; i++) {
+        let chr = iV[i].toLowerCase();
+        result += this.BIN_DICTIONARY[chr];
+     
+    }
+  
+    return result;
+}
+convertToHexa(inputValue) {
+    let result;
+
+    let arr = [];
+    let i = inputValue;
+    do {
+        arr.push(i % 16);
+        i = (i - i % 16) / 16;
+    }
+    while (i > 0);
+   
+    for (let j = 0; j < arr.length; j++) {
+        if (arr[j] > 9) {
+            arr[j] = this.TO_HEXA_DICTIONARY[arr[j]];
+            //arr[j] = arr[j].toString(16).toUpperCase();
+        } else {
+            arr[j] = arr[j];
+        }
+    }
+    arr.reverse();
+    result = "";
+    result = arr.join("");
+    return result;
+}
+
+
+
+convertToDecimal(inputValue) {
+    let result=0;
+    let numOfDigits;
+    numOfDigits = inputValue.length;
+    for (let i = 0; i < numOfDigits; i++) {
+        let chr = inputValue[i].toUpperCase();
+        let re = /[abcdef]/i;
+        if(re.test(inputValue[i])){
+            chr = this.HEXA_DICTIONARY[chr];
+        }else{
+            chr = chr;
+        }
+        result = Number(result) + Number(chr) * Math.pow(16, numOfDigits - 1 - i);
+        };
+        return result;
+    };
+
+
+}
+
+class Converter {
+    convertToBinary(inputValue) {
+        return (parseInt(inputValue, 16).toString(2)).padStart(8, '0');   
+    }
+    
+    convertToHexa(inputValue) {
+        return Math.abs(inputValue).toString(16).toLocaleUpperCase();
+    }
+    
+    convertToDecimal(inputValue) {
+        return parseInt(inputValue, 16);
+    
+    }
+}
+
+
---
+++ convertToUsingLibrary/main.js
@@ -0,0 +1,113 @@
+"use strict";
+import myConverter from './converter.js';
+const FORMAT_HEX = 1;
+const FORMAT_DEC = 2;
+
+const inputs = [1323, 4322, 2341, 134, 2, 3, 4, 45654, 3455, 9839, 748789234, '0x31432', 'AEF', '#34A'];
+inputs.forEach(function(input){
+function removeHexSignature(input) {
+    let result = '';
+    let result1 = '';
+    let re = /^([0X]|[0X]|[#])/;
+    let re2 = /^([x]|[X])/;
+    let re3 = /h$/i;
+    
+    let inputWithoutOnePrefix = input.replace(re, "");
+    result1 = inputWithoutOnePrefix.replace(re2, "");
+    result = result1.replace(re3, "");
+    return result;
+}
+
+function containsOnlyNumbers(input) {
+    let pattern = /^[0-9]*$/;
+    return pattern.test(input);
+}
+
+function detectInputFormat(input) {
+    let result = "";
+    if (containsOnlyNumbers(input)) {
+        result = FORMAT_DEC;
+    } else {
+        result = FORMAT_HEX;
+    }
+
+    return result;
+}
+
+function validateInput(input) {
+    let result = true;
+    let pattern = /^#*([a-f]|[A-F]|[0-9]|[0x]|[0X]|[hH])*$/;
+    if (pattern.test(input)) {
+        result = true;
+    } else {
+        result = false;
+    }
+    //check if input contains non-hexa & non-numeric characters and chars is one of the #, x, h.
+    return result;
+}
+
+function convert(input) {
+    let inputFormat;
+    let convertedValue;
+    let binaryValue;
+    let inputIsValid;
+    let errorMsg;
+    inputIsValid= validateInput(input);
+    
+    let converter = new myConverter(-1);
+    //todo: use base inside convertor
+    if (inputIsValid) {
+    
+        inputFormat = detectInputFormat(input);
+        if (inputFormat == FORMAT_HEX) {
+            input = removeHexSignature(input);
+            convertedValue = converter.convertToDecimal(input);
+            binaryValue = converter.convertToBinary(input);
+        } else if (inputFormat == FORMAT_DEC) {
+            convertedValue = converter.convertToHexa(input);
+            binaryValue = converter.convertToBinary(convertedValue);
+        } else {
+            errorMsg = "Unkown Input Format";
+        }
+            
+       
+    } else {
+        errorMsg = "Invalid input";
+    } 
+    let results = {
+        convertedValue: convertedValue,
+        binaryValue: binaryValue,
+        inputFormat: inputFormat,
+        errorMsg: errorMsg
+    }
+    return results;
+}
+
+
+function printResults(e){
+    e.preventDefault();
+
+        let convertedResult = convert(input);  
+        const list = document.getElementById('list');
+        
+        //Create tr element
+        const row = document.createElement('tr');                                      
+
+        //Insert td                                                                                                      
+        row.innerHTML = `                        
+            <td id>${input}</td>
+            <td>${convertedResult.convertedValue}</td>
+            <td>${convertedResult.binaryValue}</td>
+        `;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+        list.appendChild(row);                                   
+}
+
+function run() {
+    const btn = document.getElementById('btn');                                                                                                  
+    btn.addEventListener('click', printResults);
+}
+
+run(); 
+
+})                                                                                     
