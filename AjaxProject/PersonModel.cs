3ef504518b3a88f9de24f529a7a74219389f161e
siminaB <simina.baumgarten@gmail.com>
2022-08-04 15:55:50 +0300

    first commit

---
+++ AjaxProject/PersonModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LDB.Web.Models
+{
+    public class PersonModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get { 
+
+                return FirstName + " " + LastName;
+            }
+        }
+
+
+        public int Age { get; set; }
+        //public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ AjaxProject/personMvc.js
@@ -0,0 +1,38 @@
+﻿Slices.PersonMvc = {
+    
+    init: function () {
+        var me = this;
+        log("init");
+        $.post(Main.fixPath("/MVCTutorial/Convert"),
+            { id: 1 },
+            function (result) {
+                log(result);
+            }        )
+
+        //me.testFunction();
+        me.selectedRowFunction();
+        
+
+    },
+
+    //testFunction: function () {
+    //    var me = this;
+    //    me.testFunction2();
+    //    log('testFunction')
+    //},
+    //testFunction2: function () {
+
+    //},
+
+    selectedRowFunction: function () {
+        
+        var me = this;
+        $("tbody").click(function (e) {
+            var selectedRow = e.target.closest("tr");
+            console.log(selectedRow);
+        });
+    },
+    
+
+
+};
